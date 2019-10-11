0369a825f808642ccd51d24374ce279d54abd622
githumbi <githumbi74@gmail.com>
2019-10-11 18:33:22 +0300

    added functionality to the dices

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,9 +16,45 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+//use stateful widget if screen content will change
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  var leftButton = 1;
+  var rightButton = 1;
+  void changeDice() {
+    setState(() {
+      print('butotn right pressd');
+      leftButton = Random().nextInt(6) + 1;
+      rightButton = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                changeDice();
+              },
+              child: Image.asset('images/dice$leftButton.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+                onPressed: () {
+                  changeDice();
+                },
+                child: Image.asset('images/dice$rightButton.png')),
+          ),
+        ],
+      ),
+    );
   }
 }
